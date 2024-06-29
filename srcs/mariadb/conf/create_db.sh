e407ff829ab5cfd832cc33366b46113324c32d95
AniDashyan <ani.dashyan77@gmail.com>
2024-06-29 09:57:49 -0400

    mariadb Dockerfile created

---
+++ srcs/mariadb/conf/create_db.sh
@@ -0,0 +1,19 @@
+if [ ! -d "/var/lib/mysql/wordpress" ]; then
+
+        cat << EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM     mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED by '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON wiordpress.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+        # run init.sql
+        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+        rm -f /tmp/create_db.sql
+fi
